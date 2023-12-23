ea78fe059b4c2a94b95963ffe49ab490adcea068
Blazic <106445762+BlazicBlaze@users.noreply.github.com>
2023-12-23 14:34:29 +0530

    update index-script.js

---
+++ index-script.js
@@ -0,0 +1,9 @@
+function getValues()
+  {
+    let nm = document.getElementById('usernameField').value;
+    let email = document.getElementById('emailField').value;
+    let pass = document.getElementById('passwordField').value;
+    alert(nm);
+    alert(email);
+    alert(pass);
+  }

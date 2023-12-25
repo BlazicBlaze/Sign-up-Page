ef4d4b562e7c74cee191b2f843807bd32b705c42
Blazic <106445762+BlazicBlaze@users.noreply.github.com>
2023-12-25 10:43:52 +0530

    Delete the file at 'database.sql'

--- database.sql
+++
@@ -1,8 +0,0 @@
-create database blazicWeb;
-
-create table userDetails (
-  id int identity(1,1) primary key,
-  username varchar not null,
-  email varchar not null unique,
-  password varchar not null
-);

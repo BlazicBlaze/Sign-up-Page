c0cbad1a240c7bc5651b7e18f3eca598bd490494
Blazic <106445762+BlazicBlaze@users.noreply.github.com>
2023-12-28 17:17:17 +0530

    update index-script.js

--- index-script.js
+++ index-script.js
@@ -6,4 +6,11 @@ function getValues()
     
     if(nm === "" || email === "" || pass === "")
       alert("You cannot leave a field empty");
+    else
+      changeSubmitToAnchor();
+  }
+  
+  function changeSubmitToAnchor()
+  {
+    document.getElementById("submitButton").innerHTML = '<a href = "profile-page.html" id = "profilePageAnchor">Go to Profile</a>';
   }
